42e0c4c2d8c8945c4514059b78ddbe0fa4864636
Amin <aminoffline@yahoo.com>
2021-08-13 18:57:37 +0430

    works - need touch to be better

---
+++ Main.py
@@ -0,0 +1,45 @@
+from random import choices , choice
+
+class Person:
+    count = 0
+    def __init__(self,name):
+        self.name = name
+        Person.count += 1
+
+class Footballer(Person):
+    def __init__(self):
+        pass
+    def Build_team(self, list):
+        self.list = list
+        Team = [Person(i) for i in self.list]
+        return Team
+
+names = open('names.txt','r',encoding="utf8")
+list = names.readlines()
+list = list[0].split(sep="-") + list[1].split(sep='-')
+LEN = len(list)
+
+L = list
+LA =[]
+for i in range(11):
+    ch = choice(L)
+    LA.append(ch)
+    L.pop(L.index(ch))
+LB = L
+A = Footballer()
+B = Footballer()
+A_Squad= A.Build_team(LA)
+B_Squad = B.Build_team(LB)
+print('A')
+for name in A_Squad:
+    a = name.name
+    a = a.replace(" ","")
+    a = a.replace('\n',"")
+    print(a)
+print('B')
+for name in B_Squad:
+    b = name.name
+    b = b.replace(" ","")
+    b = b.replace('\n',"")
+    print(b)
+
